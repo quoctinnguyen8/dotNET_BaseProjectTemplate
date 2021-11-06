203bfce4a478beb454abde1fea43ee24195a3767
tinnq8 <nguyenquoctin9x@gmail.com>
2021-11-06 22:21:26 +0700

    init source

---
+++ BaseProjectTemplate/App.Data/Entities/AppUser.cs
@@ -0,0 +1,24 @@
+﻿using App.Data.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Entities
+{
+	public class AppUser : AppBaseEntity
+	{
+		public string Username { get; set; }
+		public string PasswordHash { get; set; }
+		public string PasswordSalt { get; set; }
+		public string FullName { get; set; }
+		public string PhoneNumber1 { get; set; }
+		public string PhoneNumber2 { get; set; }
+		public string Email { get; set; }
+		public string Address { get; set; }
+		public string Avatar { get; set; }
+		public DateTime? BlockedTo { get; set; }
+		public DateTime? BlockedBy { get; set; }
+	}
+}
---
+++ BaseProjectTemplate/App.Data/Repositories/Base/RepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Repositories.Base
+{
+	public class RepositoryBase
+	{
+		
+	}
+}

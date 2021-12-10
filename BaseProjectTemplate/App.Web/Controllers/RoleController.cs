25fb2b6feb22a4461ac59219032a0c175aa1daa4
tinnq8 <nguyenquoctin9x@gmail.com>
2021-12-10 16:29:49 +0700

    xóa ràng buộc không cần thiết

--- BaseProjectTemplate/App.Web/Controllers/RoleController.cs
+++ BaseProjectTemplate/App.Web/Controllers/RoleController.cs
@@ -32,6 +32,7 @@ namespace App.Web.Controllers
 			return View(data);
 		}
 		public IActionResult Create() => View();
+
 		[HttpPost]
 		public async Task<IActionResult> Create(RoleAddVM model)
 		{

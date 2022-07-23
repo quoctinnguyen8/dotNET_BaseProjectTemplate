5d4097eaaa556d9348266c7ce828629c0ef5b06e
quoctinnguyen8 <nguyenquoctin9x@gmail.com>
2022-07-23 22:53:46 +0700

    Viết lại chức năng của navbar bằng ViewCOmponent, loại bỏ static class

---
+++ BaseProjectTemplate/App.Web/Components/MainNavBar/NavBarViewModel.cs
@@ -0,0 +1,31 @@
+﻿using App.Share.Consts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Web.Components.MainNavBar
+{
+	public class NavBarViewModel
+	{
+		public NavBarViewModel()
+		{
+			Items = new List<MenuItem>();
+		}
+		public List<MenuItem> Items { get; set; }
+	}
+
+	public class MenuItem
+	{
+		public MenuItem()
+		{
+			Permission = AuthConst.NO_PERMISSION;
+		}
+		public string Action { get; set; }
+		public string Controller { get; set; }
+		public string DisplayText { get; set; }
+		public string Icon { get; set; }
+		public int Permission { get; set; }
+		public MenuItem[] ChildrenItems { get; set; }
+	}
+}

84df48c2bb85a2bae37518e47e8ecac02743bee8
quoctinnguyen8 <nguyenquoctin9x@gmail.com>
2022-08-06 20:31:49 +0700

    tách chức năng delete vào file riêng

--- BaseProjectTemplate/App.Data/Repositories/GenericRepository.cs
+++ BaseProjectTemplate/App.Data/Repositories/GenericRepository.cs
@@ -45,50 +45,6 @@ namespace App.Data.Repositories
 			return await _db.Set<TEntity>().AnyAsync(expr);
 		}
 
-		public virtual async Task DeleteAsync(AppEntityBase entity)
-		{
-			var now = DateTime.Now;
-			entity.DeletedDate = now;
-			if (_httpContext != null)
-			{
-				entity.UpdatedBy = CurrentUserId();
-			}
-			_db.Update(entity);
-			await _db.SaveChangesAsync();
-		}
-
-		public virtual async Task DeleteAsync<TEntity>(int id) where TEntity : AppEntityBase
-		{
-			var tableName = GetTableName<TEntity>();
-			int? updateUserId = null;
-			if (_httpContext != null)
-			{
-				updateUserId = CurrentUserId();
-			}
-			var query = $"UPDATE {tableName} SET DeletedDate = GETDATE(), UpdatedBy = {updateUserId} WHERE Id = {id}";
-			await _db.Database.ExecuteSqlRawAsync(query);
-		}
-
-		public virtual async Task HardDeleteAsync<TEntity>(int id) where TEntity : AppEntityBase
-		{
-			var tableName = GetTableName<TEntity>();
-			var deleteQuery = $"DELETE {tableName} WHERE Id = {id}";
-			LogDebugQuery(deleteQuery);
-			await _db.Database.ExecuteSqlRawAsync(deleteQuery);
-		}
-
-		public virtual async Task HardDeleteAsync<TEntity>(IEnumerable<int> ids) where TEntity : AppEntityBase
-		{
-			if (ids == null || !ids.Any())
-			{
-				throw new Exception("Danh sách ID rỗng");
-			}
-			var tableName = GetTableName<TEntity>();
-			var deleteQuery = $"DELETE {tableName} WHERE Id IN ({string.Join(',', ids)})";
-			LogDebugQuery(deleteQuery);
-			await _db.Database.ExecuteSqlRawAsync(deleteQuery);
-		}
-
 		#region Helpers
 		public WebAppDbContext DbContext { get => this._db; }
 

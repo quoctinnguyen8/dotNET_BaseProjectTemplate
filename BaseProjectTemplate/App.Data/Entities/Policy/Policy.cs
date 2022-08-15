8c1e29dd3dc7ddb17570c8a7e90788493fedbc6d
ThanhThuan2k <thuanhuynh.190800@gmail.com>
2022-08-15 11:33:25 +0700

    Tạo Policy Entity

---
+++ BaseProjectTemplate/App.Data/Entities/Policy/Policy.cs
@@ -0,0 +1,17 @@
+﻿using App.Data.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Entities.Policy
+{
+    public class Policy : AppEntityBase
+    {
+        public string Name { get; set; }
+        public string CoverImage { get; set; }
+        public string Content { get; set; }
+        public int Like { get; set; }
+    }
+}
